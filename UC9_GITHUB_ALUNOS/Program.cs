13516eb62432fa8c11155238a38efc97739367a1
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-03-02 19:50:02 -0300

    git a

---
+++ UC9_GITHUB_ALUNOS/Program.cs
@@ -0,0 +1,21 @@
+﻿string nome;
+int faltas;
+int aulas;
+int l;
+int c;
+for (int l = 0; l <= 5; l++)
+{
+    Console.Clear();
+
+    Console.WriteLine("Qual o seu nome?");
+    nome = Console.ReadLine();
+    Console.WriteLine("Qual numero de aulas?");
+    aulas = int.Parse(Console.ReadLine());
+    Console.WriteLine("Qual numero de faltas?");
+    faltas = int.Parse(Console.ReadLine()); 
+    
+}
+ for (int c = 0;c <= 5;c++)
+{
+
+}
