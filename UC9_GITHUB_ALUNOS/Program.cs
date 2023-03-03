5a70dcf978a4641271ea62301a7f34a8ad85c72f
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-03-02 21:29:48 -0300

    update trabalho uc9

--- UC9_GITHUB_ALUNOS/Program.cs
+++ UC9_GITHUB_ALUNOS/Program.cs
@@ -1,9 +1,15 @@
 ﻿string nome;
+string[,] cadastro = new string[5, 7];
 int faltas;
 int aulas;
-int l;
-int c;
-for (int l = 0; l <= 5; l++)
+int l = 0;  
+int c = 0;  
+double prova1;
+double prova2;   
+double prova3;
+double trab4;
+
+for (c = 0; c <= 5; c++)
 {
     Console.Clear();
 
@@ -12,10 +18,23 @@ for (int l = 0; l <= 5; l++)
     Console.WriteLine("Qual numero de aulas?");
     aulas = int.Parse(Console.ReadLine());
     Console.WriteLine("Qual numero de faltas?");
-    faltas = int.Parse(Console.ReadLine()); 
-    
+    faltas = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a nota da prova 1");
+    prova1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a nota da prova 2");
+    prova2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a nota da prova 3");
+    prova3 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a nota de seu trabalho");
+    trab4 = double.Parse(Console.ReadLine());
 }
- for (int c = 0;c <= 5;c++)
+for (l = 0; l<= 5; l++)
 {
-
+    for (c = 0; c <= 7; c++)
+    {
+        Console.Write(cadastro[l, c] + " - ");
+    }
+    Console.WriteLine("");
 }
+
+
